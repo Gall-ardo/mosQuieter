262ea01418388f6fb3096da0299f79471b7ee89f
İrem Esendemir <irem.esendemir@ug.bilkent.edu.tr>
2024-07-19 15:22:33 +0300

    Changed the language for the settings page.

--- app/src/main/java/com/hao/mosquieter/SettingsPage.java
+++ app/src/main/java/com/hao/mosquieter/SettingsPage.java
@@ -1,17 +1,65 @@
 package com.hao.mosquieter;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.app.Activity;
 
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class SettingsPage extends AppCompatActivity {
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+        spinner = findViewById(R.id.languageSpinner);
+        String[] languages = {getString(R.string.choose_language), getString(R.string.eng), getString(R.string.tr)};
+        ArrayAdapter<String> languagesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, languages);
+        languagesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(languagesAdapter);
+        spinner.setSelection(0);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                String item = adapterView.getItemAtPosition(position).toString();
+                // Change the language to the selected item
+                if (item.equals(getString(R.string.eng))) {
+                    setLocale(SettingsPage.this, "en");
+                }
+                else if (item.equals(getString(R.string.tr))) {
+                    setLocale(SettingsPage.this, "tr");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do nothing
+            }
+        });
 
     }
 
+    private void setLocale(Activity activity,String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+        Intent refresh = new Intent(activity, SettingsPage.class);
+        startActivity(refresh);
+        finish();
+    }
 }
